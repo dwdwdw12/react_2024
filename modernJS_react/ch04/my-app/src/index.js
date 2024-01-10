76ac27e20e35c6c87f939ea91c7045487728084b
dongwoo <dow012@naver.com>
2024-01-10 22:57:40 +0900

    ch04

---
+++ modernJS_react/ch04/my-app/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"; //모듈 import
+
+const App = () =>{
+  //return null;
+  return <h1>안녕하세요!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));  //root : index.html에 존재.
