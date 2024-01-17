900a91b0567d180b89d6defe921a66a92e56b0cb
dongwoo <dow012@naver.com>
2024-01-17 20:53:47 +0900

    ch06

--- modernJS_react/ch06/react_re_rendering/src/App.jsx
+++ modernJS_react/ch06/react_re_rendering/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState, memo, useCallback } from "react";
 import { Child1 } from "./components/Child1";
 import { Child4 } from "./components/Child4";
 
-export const App = () => {
+export const App = memo(() => {
   console.log("App 렌더링");
 
   const [num, setNum] = useState(0);
@@ -11,12 +11,19 @@ export const App = () => {
     setNum(num + 1);
   };
 
+  //const onClickReset = () => {      //하위 항목이 재렌더링됨.
+  const onClickReset = useCallback(() => {  //함수 메모이제이션
+    setNum(0);
+  }, []);
+
   return(
     <>
       <button onClick={onClickButton}>버튼</button>
       <p>{num}</p>
-      <Child1 />
+      {/* <Child1 /> */}
+      {/* Probs로 함수 설정 */}
+      <Child1 onClickReset={onClickReset}/>
       <Child4 />
     </>
   );
-};
+});
--- modernJS_react/ch06/react_re_rendering/src/components/Child1.jsx
+++ modernJS_react/ch06/react_re_rendering/src/components/Child1.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Child2 } from "./Child2";
 import { Child3 } from "./Child3";
 
@@ -7,14 +8,20 @@ const style = {
     padding: "8px"
 };
 
-export const Child1 = () => {
+//export const Child1 = memo(() => {
+export const Child1 = memo((props) => {
     console.log("Child1 렌더링");
+
+    //Props로부터 함수를 전개(분할대입)
+    const {onClickReset} = props;
 
     return(
         <div style={style}>
             <p>Child1</p>
+            {/* 전달된 함수를 실행하는 버튼 설정 */}
+            <button onClick={onClickReset}>리셋</button>
             <Child2 />
             <Child3 />
         </div>
     );
-};
+});
