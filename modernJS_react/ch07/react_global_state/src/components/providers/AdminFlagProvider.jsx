04cc0e5e59652cf3189808efb5c31d77d60f0150
dongwoo <dow012@naver.com>
2024-01-18 23:05:57 +0900

    ch07

---
+++ modernJS_react/ch07/react_global_state/src/components/providers/AdminFlagProvider.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+export const AdminFlagContext = createContext({});
+
+export const AdminFlagProvider = props => {
+    const {Children} = props;
+
+    //동작 확인을 위해 적절한 객체를 정의
+    const sampleObj = {sampleValue: "테스트"};
+
+    //AdminFlgContext 안에 Provider가 잇으므로 각각 childeren을 감싼다
+    //value 안에 글로벌로 다룰 실제 값을 설정
+    return(
+        <AdminFlagContext.Provider value={sampleObj}>
+            {Children}
+        </AdminFlagContext.Provider>
+    );
+};
