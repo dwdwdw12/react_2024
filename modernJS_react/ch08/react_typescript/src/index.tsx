1646c9f721342b7e15025a89d23ca8cd2539ef5b
dongwoo <dow012@naver.com>
2024-01-23 22:26:08 +0900

    ch08 update

--- modernJS_react/ch08/react_typescript/src/index.tsx
+++ modernJS_react/ch08/react_typescript/src/index.tsx
@@ -1,37 +1,10 @@
-import { useEffect, useState } from 'react';
-import { ListItem } from './components/ListItem';
-import axios from 'axios';
-import type { User } from './types/user';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 
-// type User = {
-//   id: number;
-//   name: string;
-//   age: number;
-//   personalColor: string;
-// };
-
-export const App = () => {
-  //얻은 사용자 정보
-  const [users, setUsers] = useState<User[]>([]);
-
-  //화면에 표시될 때 사용자 정보 얻기
-  useEffect(() => {
-    axios.get<User[]>("https://example.com/users").then((res) => {
-      setUsers(res.data);
-    });
-  }, []);
-
-  return (
-    <div>
-      {users.map((user) => (
-        <ListItem
-          id={user.id}
-          name={user.name}
-          age={user.age}
-          personalColor={user.personalColor}
-          //hobbies={user.hobbies}
-        />
-      ))}
-    </div>
-  );
-};
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
